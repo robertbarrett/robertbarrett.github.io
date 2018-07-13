28ebca8ab29e51be722281ed91e7a80ddb95d697
Robert Barrett <barrybobbit@gmail.com>
2018-07-13 00:31:33 -0400

    Create print.js

---
+++ print.js
@@ -0,0 +1,12 @@
+function printEmail() {
+	var em1 = "outlook.com"
+	var em2 = "rbarrett"
+	var str = em2 + '@' + em1
+	document.write(em2 + '@' + em1);
+}
+function printPhone() {
+	var ph1 = "265"
+	var ph2 = "613"
+	var ph3 = "3841"
+	document.write('(' + ph2 + ')' + ' ' + ph1 + '-' + ph3);
+}
